8340a6629a5eae3419b18093a4478f2b366581af
Enge-Olate <marcioalexisolate@live.com>
2025-09-15 11:08:23 -0300

    Criando mensagens de confirmação de positivo e negativo para o convite. Começando a aplicar a lógica para as mensagens.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,49 @@
-const aniversario = new Date('Sep 17, 2025 20:00:00');
-const tempoAniversario = aniversario.getTime();
+document.addEventListener("DOMContentLoaded", function () {
+  const botao = document.getElementById("button-form");
+  const botaoConfirmacao = document.getElementById("botao-confirmacao");
+  botao.addEventListener("click", () => {
+    const formulario = document.querySelector(".formulario");
+    formulario.classList.remove("formulario--is-active");
+    AOS.init({ duration: 800, once:true});
+    setTimeout(() => {
+      AOS.refresh();
+    }, 150);
+  });
+  botaoConfirmacao.addEventListener('click', ()=>{
+    const mensagemSim = document.querySelector(".positivo");
+    const mensagemNao = document.querySelector(".negativo");
+    mensagemSim.classList.remove("positivo--is-active");
+    mensagemNao.classList.remove("negativo--is-active");
+    AOS.init({ duration: 800, once:true});
+    setTimeout(() => {
+      AOS.refresh();
+    }, 150);
+  });
+  const aniversario = new Date("Sep 17, 2025 20:00:00");
+  const tempoAniversario = aniversario.getTime();
 
-function dias(){
-    return 1000 * 60 **2 *24;
-}
-function horas(){
-    return 1000 * 60**2;
-}
-function minutos(){
+  function dias() {
+    return 1000 * 60 ** 2 * 24;
+  }
+  function horas() {
+    return 1000 * 60 ** 2;
+  }
+  function minutos() {
     return 1000 * 60;
-}
-const contandoHoras = setInterval(() => {
+  }
+  const contandoHoras = setInterval(() => {
     let agora = new Date();
     let tempoAgora = agora.getTime();
     let diferenca = tempoAniversario - tempoAgora;
-    document.querySelector('h1').innerHTML=`Faltam
+    document.querySelector("h1").innerHTML = `Faltam
         ${Math.floor(diferenca / dias())}d
-        ${((diferenca % dias())/horas()).toFixed(2)}h
-        ${((diferenca % horas())/minutos()).toFixed(2)}m
-        ${Math.floor((diferenca % minutos())/ 1000)}s
-    `
-    if(diferenca < 0){
-        clearInterval(contandoHoras);
-        document.querySelector('p').innerHTML='Expirado!';
+        ${Math.floor((diferenca % dias()) / horas())}h
+        ${Math.floor((diferenca % horas()) / minutos())}m
+        ${Math.floor((diferenca % minutos()) / 1000)}s
+        `;
+    if (diferenca < 0) {
+      clearInterval(contandoHoras);
+      document.querySelector("p").innerHTML = "Expirado!";
     }
-}, 1000);
+  }, 1000);
+});
